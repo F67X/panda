751eb3b5fbb6ddc837fd1d15696787db8c161850
aiMa_ <[l-7x@qq.com]>
2019-07-26 19:02:29 +0800

    modify

--- src/components/SecondEditon/SlideNav.vue
+++ src/components/SecondEditon/SlideNav.vue
@@ -26,12 +26,10 @@
         </el-col>
         <el-col :xs="24" :lg="14" :xl="24">
           <p class="small a-5">
-            <a href="http://behance.net/tombiskup" target="_blank">Behance</a>
+            <a href="https://www.zcool.com.cn/u/15481404" target="_blank">ZCool</a>
             <br />
-            <a href="http://instagram.com/tom.biskup" target="_blank">Instagram</a>
+            <a href="https://weibo.com/u/1970173015" target="_blank">WeiBo</a>
             <br />
-            <a href="http://facebook.com/tomaszbiskupvisuals"></a>
-            <a href="http://facebook.com/tombiskup" target="_blank">Facebook</a>
           </p>
         </el-col>
       </el-row>
@@ -51,7 +49,7 @@ export default {
   }
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 .slide-nav {
   width: 52%;
   background: #fff;
@@ -95,14 +93,7 @@ export default {
 .h2 {
   font-size: 1.55rem;
   margin-bottom: 0.5rem;
-  font-family: inherit;
-  font-weight: 400;
-  color: inherit;
-  line-height: 1.8;
-}
-.h5 {
-  font-size: 1.25rem;
-  line-height: 1.3;
+  line-height: 1.5;
 }
 small,
 .small {
@@ -110,9 +101,6 @@ small,
   font-weight: normal;
   line-height: 1.5rem;
   text-align: left;
-}
-.a-5 {
-  opacity: 0.5;
 }
 p:last-child {
   margin-bottom: 0;
@@ -183,14 +171,71 @@ p:last-child {
   -webkit-transform-origin: 100% 0;
   transform-origin: 100% 0;
 }
+.menu-con {
+  pointer-events: auto;
+}
 .menu-con a.link-thick:after,
 .menu-con .link-active a:after {
   height: 2px;
   bottom: 3px;
 }
 
-a:not(.link-thick):hover:after {
+p {
+  margin-bottom: 1rem;
+}
+
+p:last-child {
+  margin-bottom: 0;
+}
+.menu-con
+  a:not(.link-thick):not(.btn):not(.mejs-horizontal-volume-slider):not(.thumb) {
+  display: inline-block;
+}
+
+.menu-con a:not(.link-thick):hover:after {
   -webkit-animation: "link-hover" 0.6s cubic-bezier(0.19, 1, 0.22, 1) 0s;
   animation: "link-hover" 0.6s cubic-bezier(0.19, 1, 0.22, 1) 0s;
+}
+
+.menu-con
+  a:not(.link-thick):not(.btn):not(.mejs-horizontal-volume-slider):not(.thumb):hover:after {
+  -webkit-animation: "link-hover" 0.6s cubic-bezier(0.19, 1, 0.22, 1) 0s;
+  animation: "link-hover" 0.6s cubic-bezier(0.19, 1, 0.22, 1) 0s;
+}
+.link-thick {
+  display: inline-block;
+}
+.link-thick:after {
+  content: "";
+  display: block;
+  position: relative;
+  height: 1px;
+  bottom: 0px;
+  background: #000;
+  -webkit-transform: scaleX(0);
+  transform: scaleX(0);
+  -webkit-transform-origin: 0 0;
+  transform-origin: 0 0;
+  -webkit-transition: all 0.6s cubic-bezier(0.19, 1, 0.22, 1);
+  transition: all 0.6s cubic-bezier(0.19, 1, 0.22, 1);
+}
+.link-thick:hover:after {
+  -webkit-transform: scaleX(1);
+  transform: scaleX(1);
+  -webkit-animation: transform 0.6s cubic-bezier(0.19, 1, 0.22, 1) 0s;
+  animation: transform 0.6s cubic-bezier(0.19, 1, 0.22, 1) 0s;
+}
+.menu-con
+  a:not(.link-thick):not(.btn):not(.mejs-horizontal-volume-slider):not(.thumb):after {
+  content: "";
+  display: block;
+  position: relative;
+  height: 1px;
+  bottom: 3px;
+  background: #000;
+  -webkit-transform: scaleX(1);
+  transform: scaleX(1);
+  -webkit-transform-origin: 100% 0;
+  transform-origin: 100% 0;
 }
 </style>
